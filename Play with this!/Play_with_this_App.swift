a3e22ec1a44b492ba608e5db93466c727945637a
Yousef <yous3fnoor@gmail.com>
2023-12-14 21:13:04 -0600

    Initial Commit

---
+++ Play with this!/Play_with_this_App.swift
@@ -0,0 +1,17 @@
+//
+//  Play_with_this_App.swift
+//  Play with this!
+//
+//  Created by Yousef Noor on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Play_with_this_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
